d174324da5c8c31dd7ae3dc4aa8db008c5559770
marinn6 <nmarin@pursuit.org>
2024-12-10 16:24:27 -0500

    Create landing page

---
+++ client/src/landingPage/landingPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./landingPage.css"
+
+const landingPage = () => {
+  return (
+    <div>
+      <div className='player-profile'>
+        <div className='profile'></div>
+        <div className='score'></div>
+      </div>
+      <div className='play-mode'>
+        <h1>Connect 4</h1>
+        <div className='play-mode_buttons'>
+          <button>Multi-player</button>
+          <button></button>
+        </div>
+        div
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default landingPage
