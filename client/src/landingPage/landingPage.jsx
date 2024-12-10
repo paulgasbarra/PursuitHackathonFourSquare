8bb2f55f14f460880e0d029eb5ff43ccc6a3502d
marinn6 <nmarin@pursuit.org>
2024-12-10 17:48:16 -0500

    nicole commit

--- client/src/landingPage/landingPage.jsx
+++ client/src/landingPage/landingPage.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./landingPage.css"
 
 const landingPage = () => {
+  const { API } = useContext(UserContext);
+  const [singlePlayer, setSinglePlayer] = useState(false)
+  const [coOp, setCoOp] = useState(false)
+  const [onlineMatch, setOnlineMatch] = useState(false)
   return (
     <div>
       <div className='player-profile'>
-        <div className='profile'></div>
-        <div className='score'></div>
+        <div className='profile'>
+          <div className="profile__username"></div>
+        </div>
+        <div className='score'>
+          <p className="score__wins">Wins: </p>
+          <p className="score__lose">Losses: </p>
+          <p className="score__ties">Ties: </p>
+        </div>
       </div>
       <div className='play-mode'>
         <h1>Connect 4</h1>
-        <div className='play-mode_buttons'>
-          <button>Multi-player</button>
-          <button></button>
+        <div className='play-mode__buttons'>
+          <button>Single Player</button>
+          <button>Local Co-Op</button>
+          <button>Online Match</button>
         </div>
-        div
-        <div></div>
-        <div></div>
+      
       </div>
     </div>
   )
