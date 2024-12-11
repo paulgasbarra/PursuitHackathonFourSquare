d7aaa0c47f098fcb3484baedbf4c08bb73b99533
Paul Gasbarra <paulgasbarra@Admins-MacBook-Air.local>
2024-12-10 19:19:21 -0500

    adjust default port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const app = require("./app.js");
 
 require("dotenv").config();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
