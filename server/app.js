edb29d520f07891e6b1a56ab09be861fe568989d
Paul Gasbarra <paulgasbarra@Admins-MacBook-Air.local>
2024-12-10 19:18:08 -0500

    another correct

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.status(404).send("Page not found");
+  res.status(404).send("The request you are looking for does not exist!");
 });
 
 module.exports = app;
